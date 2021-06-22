9c5ef134499c5904bfda4567b0268bac10f26c20
André da Maia <andre.jr91@gmail.com>
2021-06-22 16:00:55 -0300

    header recolocado

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,13 @@ export default function Header () {
       <header className={styles.header}>
         <div className="container-fluid">
           <div className="row">
+            <div className="col-6 col-md-4 col-xl-4">
+              <Link href="/"><a><img src="logo.png" alt="Nissin solutions" /></a></Link>
+            </div>
+
             <div className="col-12 col-md-8 col-xl-8 d-none d-md-block">
               <div className={styles.menu}>
+                <div className={styles.submenuInfos}>
                   <div className={styles.infos}>
                     <a href="mailto:contato@nissinsolutions.com.br">
                         <img src="email.png" alt="" />
@@ -26,13 +31,14 @@ export default function Header () {
 
                     <a href="tel:+5547988496155">+55 (47) 98849-6155</a>
                   </div>
+                </div>
 
                 <div className={styles.menuList}>
                   <Link href="/"><a>Home <div className={styles.line}></div></a></Link>
 
                   <Link href="/sobre"><a>Sobre <div className={styles.line}></div></a></Link>
 
-                  <Link href="/categorias"><a>Produtos <div className={styles.line}></div></a></Link>
+                  <Link href="/produtos"><a>Produtos <div className={styles.line}></div></a></Link>
 
                   <Link href="/contato"><a>Contato <div className={styles.line}></div></a></Link>
                 </div>
